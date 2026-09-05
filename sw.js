4631ae464acb5d5efea0a18f1eed830063c58a01
mshgroupbr <rafaelcarlos.poa@gmail.com>
2026-09-05 13:03:15 -0300

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'freelancers-cali-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(
+      names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then((res) => {
+      const resClone = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+      return res;
+    }).catch(() => caches.match(event.request))
+  );
+});
